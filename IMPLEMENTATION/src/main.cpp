0051b9e292d27c442f10b645ca6609de23dc6d05
MCLEANS <jackagwaya@gmail.com>
2020-07-23 20:26:35 +0300

    initialized system clock

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -10,10 +10,16 @@
 
 
 #include "stm32f4xx.h"
+#include "clockconfig.h"
+
+custom_libraries::clock_config system_clock;
 			
 
 int main(void)
 {
+	//initialize system clock to PLL 168MHz on AHB
+	system_clock.initialize();
+
 
 	while(1){
 
